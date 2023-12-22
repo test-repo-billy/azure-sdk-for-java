fdadc69e3d0e095dcdd1297c4484c7b796855b11
SDKAuto <sdkautomation@microsoft.com>
2023-12-22 08:02:44 +0000

    CodeGen from PR 3152 in test-repo-billy/azure-rest-api-specs Merge bd576d92da41e2d6dc2c9bf43475e86433695a9e into 9336afae9aef434bc0f382e4e0e811ac4925445a

--- sdk/servicelinker/azure-resourcemanager-servicelinker/src/samples/java/com/azure/resourcemanager/servicelinker/generated/LinkerListSamples.java
+++ sdk/servicelinker/azure-resourcemanager-servicelinker/src/samples/java/com/azure/resourcemanager/servicelinker/generated/LinkerListSamples.java
@@ -4,23 +4,23 @@
 
 package com.azure.resourcemanager.servicelinker.generated;
 
-import com.azure.core.util.Context;
-
-/** Samples for Linker List. */
+/**
+ * Samples for Linker List.
+ */
 public final class LinkerListSamples {
     /*
-     * x-ms-original-file: specification/servicelinker/resource-manager/Microsoft.ServiceLinker/stable/2022-05-01/examples/LinkList.json
+     * x-ms-original-file:
+     * specification/servicelinker/resource-manager/Microsoft.ServiceLinker/preview/2023-11-01-preview/examples/
+     * LinkerList.json
      */
     /**
-     * Sample code: LinkList.
-     *
+     * Sample code: LinkerList.
+     * 
      * @param manager Entry point to ServiceLinkerManager.
      */
-    public static void linkList(com.azure.resourcemanager.servicelinker.ServiceLinkerManager manager) {
-        manager
-            .linkers()
-            .list(
-                "subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Microsoft.Web/sites/test-app",
-                Context.NONE);
+    public static void linkerList(com.azure.resourcemanager.servicelinker.ServiceLinkerManager manager) {
+        manager.linkers().list(
+            "subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/test-rg/providers/Microsoft.Web/sites/test-app",
+            com.azure.core.util.Context.NONE);
     }
 }
