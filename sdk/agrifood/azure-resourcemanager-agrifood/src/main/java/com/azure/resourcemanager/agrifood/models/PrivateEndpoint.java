e2c7f7e313836eecdf517bdd0586775a22676f0a
SDKAuto <sdkautomation@microsoft.com>
2024-04-25 19:06:40 +0000

    CodeGen from PR 3327 in test-repo-billy/azure-rest-api-specs Merge 36208b45a5ea89e3f1f5e7ba03220f6fae35af7f into 26ae65b7cff0082d7acce6e28ac8d6739bc19745

--- sdk/agrifood/azure-resourcemanager-agrifood/src/main/java/com/azure/resourcemanager/agrifood/models/PrivateEndpoint.java
+++ sdk/agrifood/azure-resourcemanager-agrifood/src/main/java/com/azure/resourcemanager/agrifood/models/PrivateEndpoint.java
@@ -7,18 +7,26 @@ package com.azure.resourcemanager.agrifood.models;
 import com.azure.core.annotation.Immutable;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-/** The Private Endpoint resource. */
+/**
+ * The private endpoint resource.
+ */
 @Immutable
 public final class PrivateEndpoint {
     /*
-     * The ARM identifier for Private Endpoint
+     * The ARM identifier for private endpoint.
      */
     @JsonProperty(value = "id", access = JsonProperty.Access.WRITE_ONLY)
     private String id;
 
     /**
-     * Get the id property: The ARM identifier for Private Endpoint.
-     *
+     * Creates an instance of PrivateEndpoint class.
+     */
+    public PrivateEndpoint() {
+    }
+
+    /**
+     * Get the id property: The ARM identifier for private endpoint.
+     * 
      * @return the id value.
      */
     public String id() {
@@ -27,7 +35,7 @@ public final class PrivateEndpoint {
 
     /**
      * Validates the instance.
-     *
+     * 
      * @throws IllegalArgumentException thrown if the instance is not valid.
      */
     public void validate() {
